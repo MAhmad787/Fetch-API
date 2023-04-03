ae3e10607778ca9a68a3f8f8c1d43824b5de9c99
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-04-03 23:50:07 +0500

    Get data from local json file

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-const text = document.getElementById('text');
+const output = document.getElementById('text');
+
+// Get Text sample
+
 document.getElementById('getText').addEventListener('click', () => {
   fetch('sample.txt')
-    .then((res) => {
-      return res.text();
-    })
+    .then((res) => res.text())
     .then((data) => {
-      console.log(data);
-      text.innerHTML = data;
+      output.innerHTML = data;
+    });
+});
+
+// Get Users from local JSON
+document.getElementById('getUsers').addEventListener('click', () => {
+  fetch('users.json')
+    .then((res) => res.json())
+    .then((data) => {
+      let outputText = ` <h2>Users</h2>`;
+      data.forEach((item) => {
+        outputText += `
+            <ul>
+                <li>User: ${item.id} </li>
+                <li>Name ${item.name}</li>
+                <li>Age ${item.age}</li>
+            </ul>
+              `;
+      });
+      output.innerHTML = outputText;
     });
 });
