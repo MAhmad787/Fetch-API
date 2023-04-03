beaa1765516e31d6f164d8bbfce10110e564fb58
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-04-04 00:14:24 +0500

    GET posts & POST posts

--- main.js
+++ main.js
@@ -23,8 +23,45 @@ document.getElementById('getUsers').addEventListener('click', () => {
                 <li>Name ${item.name}</li>
                 <li>Age ${item.age}</li>
             </ul>
+            <hr/>
               `;
       });
       output.innerHTML = outputText;
+    });
+});
+// Get Posts from External API
+document.getElementById('getPosts').addEventListener('click', () => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((data) => {
+      let outputText = ` <h2>Posts</h2>`;
+      data.forEach((post) => {
+        outputText += `
+            <div>
+                <h3>${post.title} </h3>
+                <p>${post.body}</p>
+            </div>
+            <hr/>
+              `;
+      });
+      output.innerHTML = outputText;
+    });
+});
+// Send Posts to External API
+document.getElementById('addPosts').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const body = document.getElementById('body').value;
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({ title: title, body: body }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
     });
 });
