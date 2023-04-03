783b829328fa69b2d903dec3cd4f05defde10f46
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-04-03 23:30:12 +0500

    Can get data form local .txt file

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const text = document.getElementById('text');
 document.getElementById('getText').addEventListener('click', () => {
   fetch('sample.txt')
-    .then((response) => {
-      return response.text();
+    .then((res) => {
+      return res.text();
     })
     .then((data) => {
       console.log(data);
